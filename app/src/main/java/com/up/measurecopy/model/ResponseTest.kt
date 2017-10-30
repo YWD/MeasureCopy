7d736419b9ccd6f8e157408723267a4274f4be93
杨文冬 <wendong.yang@fuwo.com>
2017-10-30 20:18:02 +0800

    ResponseTest

---
+++ app/src/main/java/com/up/measurecopy/model/ResponseTest.kt
@@ -0,0 +1,11 @@
+package com.up.measurecopy.model
+
+/**
+ * Created by ywd on 2017/10/30.
+ */
+
+class ResponseTest<T> {
+    var message: String? = null
+    var code: Int = 0
+    var data: T? = null
+}
